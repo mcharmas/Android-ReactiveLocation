1f6ea0cf71933c7a17ab97cca6b4993772411b01
Roman Iakovlev <iakovlev@live.de>
2014-03-18 11:55:44 +0100

    Another deprecated API call removed.

--- android-reactive-location/src/main/java/pl/charmas/android/reactivelocation/observables/geocode/GeodecodeObservable.java
+++ android-reactive-location/src/main/java/pl/charmas/android/reactivelocation/observables/geocode/GeodecodeObservable.java
@@ -9,10 +9,11 @@ import java.util.List;
 
 import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
 import rx.Subscription;
 import rx.subscriptions.Subscriptions;
 
-public class GeodecodeObservable implements Observable.OnSubscribeFunc<List<Address>> {
+public class GeodecodeObservable implements Observable.OnSubscribe<List<Address>> {
     private final Context ctx;
     private final double latitude;
     private final double longitude;
@@ -30,14 +31,13 @@ public class GeodecodeObservable implements Observable.OnSubscribeFunc<List<Addr
     }
 
     @Override
-    public Subscription onSubscribe(Observer<? super List<Address>> observer) {
+    public void call(Subscriber<? super List<Address>> subscriber) {
         Geocoder geocoder = new Geocoder(ctx);
         try {
-            observer.onNext(geocoder.getFromLocation(latitude, longitude, maxResults));
-            observer.onCompleted();
+            subscriber.onNext(geocoder.getFromLocation(latitude, longitude, maxResults));
+            subscriber.onCompleted();
         } catch (IOException e) {
-            observer.onError(e);
+            subscriber.onError(e);
         }
-        return Subscriptions.empty();
     }
 }
