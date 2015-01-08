ff9ab9a3a229ec25e83083dba2fc6594e54317d7
cyrix <cyrixmorten@gmail.com>
2015-01-08 23:07:21 +0100

    Removed static reference
    added selective activity example

--- sample/src/main/java/pl/charmas/android/reactivelocation/sample/MainActivity.java
+++ sample/src/main/java/pl/charmas/android/reactivelocation/sample/MainActivity.java
@@ -35,15 +35,21 @@ public class MainActivity extends ActionBarActivity {
     private TextView updatableLocationView;
     private TextView addressLocationView;
     private TextView currentActivityView;
+    private TextView activity_selective_view;
 
     private Observable<Location> lastKnownLocationObservable;
     private Observable<Location> locationUpdatesObservable;
     private Observable<DetectedActivity> activityObservable;
+    private Observable<DetectedActivity> selectiveActivityObservable;
+
 
     private Subscription lastKnownLocationSubscription;
     private Subscription updatableLocationSubscription;
     private Subscription addressSubscription;
     private Subscription activitySubscription;
+    private Subscription selectiveActivitySubscription;
+
+    private DetectedActivity mLastSelectedActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +60,8 @@ public class MainActivity extends ActionBarActivity {
         lastKnownLocationView = (TextView) findViewById(R.id.last_known_location_view);
         updatableLocationView = (TextView) findViewById(R.id.updated_location_view);
         addressLocationView = (TextView) findViewById(R.id.address_for_location_view);
-        currentActivityView = (TextView) findViewById(R.id.most_recent_activity_view);
+        currentActivityView = (TextView) findViewById(R.id.activity_recent_view);
+        activity_selective_view = (TextView) findViewById(R.id.activity_selective_view);
 
         locationProvider = new ReactiveLocationProvider(getApplicationContext());
         lastKnownLocationObservable = locationProvider.getLastKnownLocation();
@@ -67,7 +74,9 @@ public class MainActivity extends ActionBarActivity {
         );
 
         activityObservable = locationProvider.getDetectedActivity(0);
+        selectiveActivityObservable = locationProvider.getDetectedActivity(0);
 
+        mLastSelectedActivity = new DetectedActivity(DetectedActivity.UNKNOWN, 0);
 
     }
 
@@ -142,6 +151,40 @@ public class MainActivity extends ActionBarActivity {
                 })
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new DisplayTextOnViewAction(currentActivityView));
+
+        selectiveActivitySubscription = selectiveActivityObservable.doOnError(new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                String message = "Error on activitySubscription: " + throwable.getMessage();
+                Log.e(TAG, message, throwable);
+            }
+        }).onErrorReturn(new Func1<Throwable, DetectedActivity>() {
+            @Override
+            public DetectedActivity call(Throwable throwable) {
+                return new DetectedActivity(DetectedActivity.UNKNOWN,0);
+            }
+        }).filter(new Func1<DetectedActivity, Boolean>() {
+            @Override
+            public Boolean call(DetectedActivity detectedActivity) {
+                boolean highConfidence = detectedActivity.getConfidence() > 50;
+                boolean notUnknown = detectedActivity.getType() != DetectedActivity.UNKNOWN;
+
+                boolean isNewActivity = detectedActivity.getType() != mLastSelectedActivity.getType();
+                boolean hasHigherConfidence = detectedActivity.getConfidence() > mLastSelectedActivity.getConfidence();
+                return highConfidence && notUnknown && (isNewActivity || hasHigherConfidence);
+            }
+        }).subscribe(new Action1<DetectedActivity>() {
+            @Override
+            public void call(DetectedActivity detectedActivity) {
+                Log.i(TAG, "Activity changed or increased in confidence:");
+                String text =
+                "Last activity: " + ActivityUtils.getNameFromType(mLastSelectedActivity.getType()) + " confidence: " + mLastSelectedActivity.getConfidence() + "\n" +
+                "New activity: " + ActivityUtils.getNameFromType(detectedActivity.getType()) + " confidence: " + detectedActivity.getConfidence();
+                mLastSelectedActivity = detectedActivity;
+                new DisplayTextOnViewAction(activity_selective_view).call(text);
+            }
+        });
+
         super.onStart();
     }
 
@@ -150,6 +193,7 @@ public class MainActivity extends ActionBarActivity {
         unSubscribeIfNotNull(updatableLocationSubscription);
         unSubscribeIfNotNull(addressSubscription);
         unSubscribeIfNotNull(activitySubscription);
+        unSubscribeIfNotNull(selectiveActivitySubscription);
         super.onStop();
     }
 
