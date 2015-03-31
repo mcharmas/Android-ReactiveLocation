00afaf8d4ab40795100d8d3ab7bc9234d3cbde82
Michał Charmas <michal@charmas.pl>
2015-03-31 18:26:51 +0200

    Removed own implementation of EditText observable from sample.

--- sample/src/main/java/pl/charmas/android/reactivelocation/sample/utils/TextObservable.java
+++ sample/src/main/java/pl/charmas/android/reactivelocation/sample/utils/TextObservable.java
@@ -1,55 +1,22 @@
 package pl.charmas.android.reactivelocation.sample.utils;
 
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.widget.EditText;
 
 import rx.Observable;
-import rx.Subscriber;
-import rx.functions.Action0;
-import rx.subscriptions.Subscriptions;
+import rx.android.widget.OnTextChangeEvent;
+import rx.android.widget.WidgetObservable;
+import rx.functions.Func1;
 
 public final class TextObservable {
     private TextObservable() {
     }
 
     public static Observable<String> create(EditText editText) {
-        return Observable.create(new TextWatcherOnSubscribe(editText));
-    }
-
-    private static class TextWatcherOnSubscribe implements Observable.OnSubscribe<String> {
-        private final EditText editText;
-
-        private TextWatcherOnSubscribe(EditText editText) {
-            this.editText = editText;
-        }
-
-        @Override
-        public void call(final Subscriber<? super String> subscriber) {
-            final TextWatcher watcher = new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    subscriber.onNext(s.toString());
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-
-                }
-            };
-            editText.addTextChangedListener(watcher);
-            subscriber.onNext(editText.getText().toString());
-            subscriber.add(Subscriptions.create(new Action0() {
-                @Override
-                public void call() {
-                    editText.removeTextChangedListener(watcher);
-                }
-            }));
-        }
+        return WidgetObservable.text(editText, true).map(new Func1<OnTextChangeEvent, String>() {
+            @Override
+            public String call(OnTextChangeEvent onTextChangeEvent) {
+                return onTextChangeEvent.text().toString();
+            }
+        });
     }
 }
